91b5a9cfa96e7cb3607401d83b1a3be6a1f90e39
ziur <ruiz.ac@gmail.com>
2021-11-22 20:04:07 -0400

    Added unit tests

---
+++ src/test/java/com/poc/gitactions/model/PersonTest.java
@@ -0,0 +1,25 @@
+package com.poc.gitactions.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+
+    Person person1;
+
+    @BeforeEach
+    void setUp() {
+        person1 = new Person("Bruno", "Dias", "bd@bat.man");
+    }
+
+    @Test
+    void getFirstName() {
+        Assertions.assertEquals("Bruno", person1.getFirstName());
+    }
+
+    @Test
+    void getEmail() {
+        Assertions.assertEquals("bd@bat.man", person1.getEmail());
+    }
+}
